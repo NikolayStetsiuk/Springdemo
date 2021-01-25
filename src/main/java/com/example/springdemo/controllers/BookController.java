d69a6f42a6841bd7b8f59d8a17e23edb84f4aa62
NikolayStetsiuk <mikstt777@gmail.com>
2021-01-25 21:09:12 +0200

    return to work variant

--- src/main/java/com/example/springdemo/controllers/BookController.java
+++ src/main/java/com/example/springdemo/controllers/BookController.java
@@ -1,6 +1,5 @@
 package com.example.springdemo.controllers;
 
-import com.example.springdemo.repozitory.BookRep;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path = "index")
 public class BookController {
 
-    private BookRep bookRep;
 
     @RequestMapping("/index")
 
