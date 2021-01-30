c59da9349fb57b93a98719811a93c2e93f937869
NikolayStetsiuk <mikstt777@gmail.com>
2021-01-30 20:31:51 +0200

    ordered converter ModelToEntity

--- src/main/java/com/example/springdemo/converters/ModelToEntity.java
+++ src/main/java/com/example/springdemo/converters/ModelToEntity.java
@@ -3,12 +3,20 @@ package com.example.springdemo.converters;
 import com.example.springdemo.entity.Book;
 import com.example.springdemo.viewModel.BookModel;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.util.StringUtils;
 
 public class ModelToEntity implements Converter<BookModel, Book> {
     @Override
     public Book convert(BookModel bookModel) {
         Book book = new Book();
 
-        return null;
+        if (bookModel.getId() != null  && !StringUtils.isEmpty(bookModel.getId())) {
+            book.setId(new Long(bookModel.getId()));
+        }
+        book.setAuthor(bookModel.getAuthor());
+        book.setImageUrl(bookModel.getImageUrl());
+        book.setTitle(bookModel.getTitle());
+
+        return book;
     }
 }
