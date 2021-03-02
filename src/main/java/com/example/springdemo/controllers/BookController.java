cf122984235b1f7f598aad13395965ee4fd131fb
NikolayStetsiuk <mikstt777@gmail.com>
2021-03-02 21:39:29 +0200

    add mustache, over do book coontrollr more simplar

--- src/main/java/com/example/springdemo/controllers/BookController.java
+++ src/main/java/com/example/springdemo/controllers/BookController.java
@@ -8,11 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class BookController {
@@ -28,6 +27,11 @@ public class BookController {
     @Autowired
     public void setEntityToModel(EntityToModel entityToModel) {
         this.entityToModel = entityToModel;
+    }
+
+    @GetMapping("/book/greeting")
+    public String greeting(){
+        return "book/greeting";
     }
 
     @RequestMapping("/")
